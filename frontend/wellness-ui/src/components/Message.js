415c47635523e4f65b701f8988d5486ccf478362
Parashuram Kateegar <pkateegar@welldocinc.com>
2025-02-21 19:03:32 +0530

    inital commit

---
+++ frontend/wellness-ui/src/components/Message.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Message = ({ text, sender, sentiment }) => {
+    const getSentimentColor = (sentiment) => {
+        switch (sentiment) {
+            case "positive":
+                return "green";
+            case "negative":
+                return "red";
+            case "neutral":
+                return "gray";
+            default:
+                return "black";
+        }
+    };
+
+    return (
+        <div className={'message ${sender}'}>
+            <p style={{ color: getSentimentColor(sentiment) }}>{text}</p>
+        </div>
+    );
+};
+
+export default Message;
